4b220f825fc62882ae6f104c31e9608ea2dbeed7
Ikeda-Togo <s17c1009mk@s.chibakoudai.jp>
2020-03-21 10:18:40 -0700

    update

--- pyqt_learn/pub_sub.py
+++ pyqt_learn/pub_sub.py
@@ -4,16 +4,17 @@ import lcm
 from exlcm import example_t
 def my_handler(channel, data):
     msg = example_t.decode(data)
-    '''
+    
     print("Received message on channel \"%s\"" % channel)
-    print("   timestamp   = %s" % str(msg.timestamp))
+    print("   mode   = %s" % str(msg.mode))
+    '''
     print("   position    = %s" % str(msg.position))
     print("   orientation = %s" % str(msg.orientation))
     print("   ranges: %s" % str(msg.ranges))
     print("   name        = '%s'" % msg.name)
     print("   enabled     = %s" % str(msg.enabled))
     '''
-    print(msg.name)
+    #print(msg.name)
     #print("")
 
 def subscribe_handler(handle):
@@ -21,7 +22,7 @@ def subscribe_handler(handle):
         handle()
 
 msg = example_t()
-msg.timestamp = 0
+msg.mode = 0
 msg.position = (1, 2, 3)
 msg.orientation = (1, 0, 0, 0)
 msg.ranges = range(15)
@@ -37,8 +38,8 @@ print("EXAMPLEチャンネルを読んでTAMAGOチャンネルに送る")
 try:
     while True:
         #print("hi")
-        msg.name =input()
-        lc.publish("TAMAGO", msg.encode())
-        time.sleep(2)
+        msg.mode =int(input("number only type:"))
+        lc.publish("EXAMPLE", msg.encode())
+        #time.sleep(2)
 except KeyboardInterrupt:
     pass
